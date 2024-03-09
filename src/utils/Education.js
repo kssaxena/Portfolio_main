4bdc8c488bda67d7fb620ed40a15b7822f29ff6b
Kshitij saxena <kshitijsaxena9@gmail.com>
2024-03-09 23:37:22 +0530

    Deletion

--- src/utils/Education.js
+++
@@ -1,7 +0,0 @@
-export const Education =[
-    {
-        id: 1,
-        course_name:"bachelor in computer science",
-        description: "Pursuing Btech CSE from Usha Martin university"
-    }
-];
