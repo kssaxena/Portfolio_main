e668cc47bf760e80b63c4db14499df499c27b4c2
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2024-03-11 15:24:25 +0530

    Modification

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,14 +3,17 @@ import { images } from "../utils/Images";
 
 const Contact = () => {
   return (
-    <div className="flex w-full justify-evenly bg-gray-200">
-      <div className="p-14">
-        <img src={images[2].url} className="rounded-full w-80" />
+    <div>
+      <h1 className="bg-gray-200 text-center text-red-500 text-4xl h-fit p-10 font-semibold">Drop Your Query here...</h1>
+      <div className="flex w-full justify-evenly bg-gray-200">
+        <div className="p-14">
+          <img src={images[2].url} className="rounded-full w-80" />
+        </div>
+        <div className="w-1/2">
+          <Form_contact />
+        </div>
+        {/* <h1>contact</h1> */}
       </div>
-      <div className="w-1/2">
-      <Form_contact/>
-      </div>
-      {/* <h1>contact</h1> */}
     </div>
   );
 };
